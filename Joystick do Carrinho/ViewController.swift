3a7a12b7128d429e2e2e7313fb57ef4105d2c892
Alan Rabelo Martins <alanrabelo@icloud.com>
2017-03-22 10:34:39 -0300

    Added outlets

--- Joystick do Carrinho/ViewController.swift
+++ Joystick do Carrinho/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func moveFront(_ sender: UIButton) {
+    }
+    
+    @IBAction func moveRight(_ sender: UIButton) {
+    }
+    @IBAction func moveLeft(_ sender: UIButton) {
+    }
+    
+    @IBAction func moveBackwards(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
